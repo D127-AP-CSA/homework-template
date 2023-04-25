6b81c596bb60809ca1c25ab256a06bada6af793d
brettveitch <blveitch@comcast.net>
2023-04-25 13:55:24 -0500

    change bound to 15

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main{
     public static void main(String[] args) {
         int[] arr = new int[5];
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int) (Math.random() * 20);
+            arr[i] = (int) (Math.random() * 10);
         }
         System.out.println(Arrays.toString(arr));
     }
